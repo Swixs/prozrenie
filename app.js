59e3034a0a4257905481abdfc369986b9febe444
Nikita Knutarev <nekitigroman3000@gmail.com>
2023-09-30 20:52:47 +0200

    micro-work

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const slider = document.getElementById("mySlider");
 const sliderValue = document.getElementById("sliderValue");
 
+sliderValue.innerHTML = "0";
 // Обработчик изменения значения ползунка
-slider.addEventListener("input", function() {
-    sliderValue.innerText = slider.value;
+slider.addEventListener("input", function() {     
+    sliderValue.innerHTML = slider.value === "0"
+        ? "0"
+        : `${slider.value} 000`
 });
+
